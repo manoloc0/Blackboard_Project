ca2848185be849a57dce5b56788ce2ada45df64f
Tyler Cadenas <tyler@forthrightfilms.com>
2024-01-29 00:36:38 -0500

    Updated Visuals now. Working TPOC.

--- Blackboard_Multi/Blackboard_Multi/AppColors.swift
+++ Blackboard_Multi/Blackboard_Multi/AppColors.swift
@@ -2,8 +2,12 @@
 import SwiftUI
 
 struct AppColors {
+    static let lightPink = Color(red: 1, green: 0.62, blue: 0.80, opacity: 1.0)
     static let pink = Color(red: 1, green: 0.49411, blue: 0.72549, opacity: 1.0)
+    static let lightBlue = Color(red: 0.80, green: 0.99, blue: 1.0, opacity: 1.0)
     static let blue = Color(red: 0.48, green: 0.99, blue: 1.0, opacity: 1.0)
+    static let lightYellow = Color(red: 0.99, green: 1.0, blue: 0.85, opacity: 1.0)
     static let yellow = Color(red: 0.99, green: 1.0, blue: 0.61, opacity: 1.0)
+
 }
 
